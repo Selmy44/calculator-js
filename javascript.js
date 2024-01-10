e0775c043eaa89d08cff2a0258641db849a959aa
Selmy NSHUTI <146110265+Selmy44@users.noreply.github.com>
2024-01-10 06:36:39 +0200

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,31 @@
+function add() {
+  var num1 = parseFloat(document.getElementById('num1').value);
+  var num2 = parseFloat(document.getElementById('num2').value);
+  var result = num1 + num2;
+  document.getElementById('result').innerHTML = 'Result: ' + result;
+}
+
+function subtract() {
+  var num1 = parseFloat(document.getElementById('num1').value);
+  var num2 = parseFloat(document.getElementById('num2').value);
+  var result = num1 - num2;
+  document.getElementById('result').innerHTML = 'Result: ' + result;
+}
+
+function multiply() {
+  var num1 = parseFloat(document.getElementById('num1').value);
+  var num2 = parseFloat(document.getElementById('num2').value);
+  var result = num1 * num2;
+  document.getElementById('result').innerHTML = 'Result: ' + result;
+}
+
+function divide() {
+  var num1 = parseFloat(document.getElementById('num1').value);
+  var num2 = parseFloat(document.getElementById('num2').value);
+  if (num2 === 0) {
+    document.getElementById('result').innerHTML = 'Cannot divide by zero';
+  } else {
+    var result = num1 / num2;
+    document.getElementById('result').innerHTML = 'Result: ' + result;
+  }
+}
