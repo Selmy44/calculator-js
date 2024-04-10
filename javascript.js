37604847ca80c159767eb247776c40b3eaba482b
Selmy <146110265+Selmy44@users.noreply.github.com>
2024-04-10 20:26:41 +0200

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,6 +1,23 @@
-function myFunction(){
-    let a=3;
-    let b=3;
-    let c=a+b;
-    document.getElementById("demo").innerHTML=c;
+function myFunction(a, b){
+    return a + b;
 }
+let x=myFunction(5, 5);
+document.getElementById("demo").innerHTML = x;
+
+
+function.myFunction(a, b){
+    // Function returns the product of a and b
+    return a * b;
+}
+// Function is called, the return value will end up in x
+let x=myFunction(5, 6);
+document.getElementById("demo").innerHTML = x;
+
+//An Example
+function toCelsius(f) {
+    return (5/9) * (f-32);
+  }
+  
+  let value = toCelsius(77);
+  document.getElementById("well1").innerHTML = value;
+  
