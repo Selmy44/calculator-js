c96dd6e17c994866b5b42539c82dc9d43827729e
Selmy <146110265+Selmy44@users.noreply.github.com>
2024-07-03 17:17:35 +0200

    Add files via upload

--- learn.js
+++ learn.js
@@ -32,7 +32,9 @@
         }
         let x = good(3);
         document.getElementById("well").innerHTML= x;
-        
+        const good2=(a)=>{
+                    return a;
+        }
         function toCelsius(f) {
             return (5/9) * (f-32);
           }
