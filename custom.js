4c83826efb6f1579c7cebd6a61e6369437db195d
Selmy <146110265+Selmy44@users.noreply.github.com>
2024-12-04 07:55:38 +0200

    Add files via upload

--- custom.js
+++ custom.js
@@ -1,13 +1,55 @@
-// to get current year
-function getYear() {
-    var currentDate = new Date();
-    var currentYear = currentDate.getFullYear();
-    document.querySelector("#displayYear").innerHTML = currentYear;
-}
-getYear();
 
-// nav menu 
-function openNav() {
-    document.getElementById("myNav").classList.toggle("menu_width")
-    document.querySelector(".custom_menu-btn").classList.toggle("menu_btn-style")
-}
+  $(function () {
+    'use strict'
+
+    // MENU
+    $('.navbar .nav-link').on('click',function(){
+        $(".navbar-collapse").collapse('hide');
+    });
+
+    $(window).on('scroll', function() {     
+                                
+        /*----------------------------------------------------*/
+        /*  Navigtion Menu Scroll
+        /*----------------------------------------------------*/    
+        
+        var b = $(window).scrollTop();
+        
+        if( b > 72 ){       
+            $(".navbar").addClass("scroll");
+        } else {
+            $(".navbar").removeClass("scroll");
+        }               
+    });
+
+    // TESTIMONIALS CAROUSEL
+    $('#testimonials-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            900:{
+                items:2,
+            },
+            1200:{
+                items:3,
+                loop:false
+            }
+        }
+    })
+
+    // SMOOTHSCROLL
+    $(function() {
+      $('.navbar .nav-link').on('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 49
+        }, 1000);
+        event.preventDefault();
+      });
+    });   
+     
+  });
