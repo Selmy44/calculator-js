8c402202afd6854926e2d49a73cfb168b2810e40
Selmy <146110265+Selmy44@users.noreply.github.com>
2025-02-11 16:34:35 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,287 +1,82 @@
-const startBtn = document.getElementById("start-btn");
-const canvas = document.getElementById("canvas");
-const startScreen = document.querySelector(".start-screen");
-const checkpointScreen = document.querySelector(".checkpoint-screen");
-const checkpointMessage = document.querySelector(".checkpoint-screen > p");
-const ctx = canvas.getContext("2d");
-canvas.width = innerWidth;
-canvas.height = innerHeight;
-const gravity = 0.5;
-let isCheckpointCollisionDetectionActive = true;
+let price = 1.87;
+let cid = [
+    ['PENNY', 1.01],
+    ['NICKEL', 2.05],
+    ['DIME', 3.1],
+    ['QUARTER', 4.25],
+    ['ONE', 90],
+    ['FIVE', 55],
+    ['TEN', 20],
+    ['TWENTY', 60],
+    ['ONE HUNDRED', 100]
+];
 
-const proportionalSize = (size) => {
-  return innerHeight < 500 ? Math.ceil((size / 500) * innerHeight) : size;
-}
-
-class Player {
-  constructor() {
-    this.position = {
-      x: proportionalSize(10),
-      y: proportionalSize(400),
-    };
-    this.velocity = {
-      x: 0,
-      y: 0,
-    };
-    this.width = proportionalSize(40);
-    this.height = proportionalSize(40);
-  }
-  draw() {
-    ctx.fillStyle = "#99c9ff";
-    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
-  }
-  
-  update() {
-    this.draw();
-    this.position.x += this.velocity.x;
-    this.position.y += this.velocity.y;
-
-    if (this.position.y + this.height + this.velocity.y <= canvas.height) {
-      if (this.position.y < 0) {
-        this.position.y = 0;
-        this.velocity.y = gravity;
-      }
-      this.velocity.y += gravity;
-    } else {
-      this.velocity.y = 0;
-    }
-
-    if (this.position.x < this.width) {
-      this.position.x = this.width;
-    }
-
-    if (this.position.x >= canvas.width - this.width * 2) {
-      this.position.x = canvas.width - this.width * 2;
-    }
-  }
-}
-
-class Platform {
-  constructor(x, y) {
-    this.position = {
-      x,
-      y,
-    };
-    this.width = 200;
-    this.height = proportionalSize(40);
-  }
-  draw() {
-    ctx.fillStyle = "#acd157";
-    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
-  }
-}
-
-class CheckPoint {
-  constructor(x, y, z) {
-    this.position = {
-      x,
-      y,
-    };
-    this.width = proportionalSize(40);
-    this.height = proportionalSize(70);
-    this.claimed = false;
-  };
-
-  draw() {
-    ctx.fillStyle = "#f1be32";
-    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
-  }
-  claim() {
-    this.width = 0;
-    this.height = 0;
-    this.position.y = Infinity;
-    this.claimed = true;
-  }
+const currencyUnit = {
+    "PENNY": 0.01,
+    "NICKEL": 0.05,
+    "DIME": 0.1,
+    "QUARTER": 0.25,
+    "ONE": 1,
+    "FIVE": 5,
+    "TEN": 10,
+    "TWENTY": 20,
+    "ONE HUNDRED": 100
 };
 
-const player = new Player();
-
-const platformPositions = [
-  { x: 500, y: proportionalSize(450) },
-  { x: 700, y: proportionalSize(400) },
-  { x: 850, y: proportionalSize(350) },
-  { x: 900, y: proportionalSize(350) },
-  { x: 1050, y: proportionalSize(150) },
-  { x: 2500, y: proportionalSize(450) },
-  { x: 2900, y: proportionalSize(400) },
-  { x: 3150, y: proportionalSize(350) },
-  { x: 3900, y: proportionalSize(450) },
-  { x: 4200, y: proportionalSize(400) },
-  { x: 4400, y: proportionalSize(200) },
-  { x: 4700, y: proportionalSize(150) },
-];
-
-const platforms = platformPositions.map(
-  (platform) => new Platform(platform.x, platform.y)
-);
-
-const checkpointPositions = [
-  { x: 1170, y: proportionalSize(80), z: 1 },
-  { x: 2900, y: proportionalSize(330), z: 2 },
-  { x: 4800, y: proportionalSize(80), z: 3 },
-];
-
-const checkpoints = checkpointPositions.map(
-  (checkpoint) => new CheckPoint(checkpoint.x, checkpoint.y, checkpoint.z)
-);
-
-const animate = () => {
-  requestAnimationFrame(animate);
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  platforms.forEach((platform) => {
-    platform.draw();
-  });
-
-  checkpoints.forEach(checkpoint => {
-    checkpoint.draw();
-  });
-
-  player.update();
-
-  if (keys.rightKey.pressed && player.position.x < proportionalSize(400)) {
-    player.velocity.x = 5;
-  } else if (keys.leftKey.pressed && player.position.x > proportionalSize(100)) {
-    player.velocity.x = -5;
-  } else {
-    player.velocity.x = 0;
-
-    if (keys.rightKey.pressed && isCheckpointCollisionDetectionActive) {
-      platforms.forEach((platform) => {
-        platform.position.x -= 5;
-      });
-
-      checkpoints.forEach((checkpoint) => {
-        checkpoint.position.x -= 5;
-      });
+document.getElementById("purchase-btn").addEventListener("click", function () {
+    let cash = parseFloat(document.getElementById("cash").value);
+    let changeDue = cash - price;
     
-    } else if (keys.leftKey.pressed && isCheckpointCollisionDetectionActive) {
-      platforms.forEach((platform) => {
-        platform.position.x += 5;
-      });
-
-      checkpoints.forEach((checkpoint) => {
-        checkpoint.position.x += 5;
-      });
-    }
-  }
-
-  platforms.forEach((platform) => {
-    const collisionDetectionRules = [
-      player.position.y + player.height <= platform.position.y,
-      player.position.y + player.height + player.velocity.y >= platform.position.y,
-      player.position.x >= platform.position.x - player.width / 2,
-      player.position.x <=
-        platform.position.x + platform.width - player.width / 3,
-    ];
-
-    if (collisionDetectionRules.every((rule) => rule)) {
-      player.velocity.y = 0;
-      return;
+    if (cash < price) {
+        alert("Customer does not have enough money to purchase the item");
+        return;
+    } 
+    
+    if (cash === price) {
+        document.getElementById("change-due").innerText = "No change due - customer paid with exact cash";
+        return;
     }
 
-    const platformDetectionRules = [
-      player.position.x >= platform.position.x - player.width / 2,
-      player.position.x <=
-        platform.position.x + platform.width - player.width / 3,
-      player.position.y + player.height >= platform.position.y,
-      player.position.y <= platform.position.y + platform.height,
-    ];
+    let totalCID = cid.reduce((sum, [_, amount]) => sum + amount, 0);
+    totalCID = parseFloat(totalCID.toFixed(2));
 
-    if (platformDetectionRules.every(rule => rule)) {
-      player.position.y = platform.position.y + player.height;
-      player.velocity.y = gravity;
-    };
-  });
+    if (totalCID < changeDue) {
+        document.getElementById("change-due").innerText = "Status: INSUFFICIENT_FUNDS";
+        return;
+    }
 
-  checkpoints.forEach((checkpoint, index, checkpoints) => {
-    const checkpointDetectionRules = [
-      player.position.x >= checkpoint.position.x,
-      player.position.y >= checkpoint.position.y,
-      player.position.y + player.height <=
-        checkpoint.position.y + checkpoint.height,
-      isCheckpointCollisionDetectionActive,
-      player.position.x - player.width <=
-        checkpoint.position.x - checkpoint.width + player.width * 0.9,
-      index === 0 || checkpoints[index - 1].claimed === true,
-    ];
+    let changeArray = [];
+    let remainingChange = changeDue;
 
-    if (checkpointDetectionRules.every((rule) => rule)) {
-      checkpoint.claim();
+    for (let i = cid.length - 1; i >= 0; i--) {
+        let [unit, amount] = cid[i];
+        let unitValue = currencyUnit[unit];
+        let amountToGive = 0;
 
+        while (remainingChange >= unitValue && amount > 0) {
+            remainingChange -= unitValue;
+            amount -= unitValue;
+            amountToGive += unitValue;
+            remainingChange = parseFloat(remainingChange.toFixed(2));
+        }
 
-      if (index === checkpoints.length - 1) {
-        isCheckpointCollisionDetectionActive = false;
-        showCheckpointScreen("You reached the final checkpoint!");
-        movePlayer("ArrowRight", 0, false);
-      }else if(player.position.x >= checkpoint.position.x && player.position.x <= checkpoint.position.x + 40){
-        showCheckpointScreen("You reached a checkpoint!")
-      }
+        if (amountToGive > 0) {
+            changeArray.push([unit, parseFloat(amountToGive.toFixed(2))]);
+        }
+    }
 
+    let totalChangeGiven = changeArray.reduce((sum, [_, amount]) => sum + amount, 0);
+    totalChangeGiven = parseFloat(totalChangeGiven.toFixed(2));
 
-    };
-  });
-}
+    if (totalChangeGiven < changeDue) {
+        document.getElementById("change-due").innerText = "Status: INSUFFICIENT_FUNDS";
+        return;
+    }
 
+    if (totalChangeGiven === totalCID) {
+        document.getElementById("change-due").innerText = `Status: CLOSED ${changeArray.map(([unit, amount]) => `${unit}: $${amount}`).join(" ")}`;
+        return;
+    }
 
-const keys = {
-  rightKey: {
-    pressed: false
-  },
-  leftKey: {
-    pressed: false
-  }
-};
-
-const movePlayer = (key, xVelocity, isPressed) => {
-  if (!isCheckpointCollisionDetectionActive) {
-    player.velocity.x = 0;
-    player.velocity.y = 0;
-    return;
-  }
-
-  switch (key) {
-    case "ArrowLeft":
-      keys.leftKey.pressed = isPressed;
-      if (xVelocity === 0) {
-        player.velocity.x = xVelocity;
-      }
-      player.velocity.x -= xVelocity;
-      break;
-    case "ArrowUp":
-    case " ":
-    case "Spacebar":
-      player.velocity.y -= 8;
-      break;
-    case "ArrowRight":
-      keys.rightKey.pressed = isPressed;
-      if (xVelocity === 0) {
-        player.velocity.x = xVelocity;
-      }
-      player.velocity.x += xVelocity;
-  }
-}
-
-const startGame = () => {
-  canvas.style.display = "block";
-  startScreen.style.display = "none";
-  animate();
-}
-
-const showCheckpointScreen = (msg) => {
-  checkpointScreen.style.display = "block";
-  checkpointMessage.textContent = msg;
-  if (isCheckpointCollisionDetectionActive) {
-    setTimeout(() => (checkpointScreen.style.display = "none"), 2000);
-  }
-};
-
-startBtn.addEventListener("click", startGame);
-
-window.addEventListener("keydown", ({ key }) => {
-  movePlayer(key, 8, true);
-});
-
-window.addEventListener("keyup", ({ key }) => {
-  movePlayer(key, 0, false);
+document.getElementById("change-due").innerText = `Status: OPEN ${changeArray.map(([unit, amount]) => `${unit}: $${amount}`).join(" ")}`;
 });
