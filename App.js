d94715dd62c01fd6d8d234e83370059b4868726d
Selmy <146110265+Selmy44@users.noreply.github.com>
2025-07-21 22:59:09 +0200

    Add files via upload

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { auth } from './firebase'; // Import auth from your Firebase configuration
 import './App.css';
 import HomeScreen from './screens/HomeScreen';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import LoginScreen from './screens/LoginScreen';
+import { useDispatch, useSelector } from 'react-redux';
+// import { selectUser } from './features/userSlice';
+import { login, logout, selectUser } from './features/userSlice';
+import ProfileScreen from './screens/ProfileScreen';
 
 
 function App() {
-  const user = null;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+useEffect(() => {
+  const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+    if (userAuth) {
+      dispatch(
+        login({
+        uid: userAuth.uid,
+        email: userAuth.email,
+      }));
+    } else {
+      // User is logged out
+      dispatch(logout())
+    }
+  });
+
+  return unsubscribe;
+}, [dispatch]);
 
   return (
     <div className="app">
@@ -15,6 +38,10 @@ function App() {
         <LoginScreen />
       ): (
         <Switch>
+          <Route path = '/profile'>
+            <ProfileScreen />
+          </Route>
+
           <Route exact path="/">
             <HomeScreen/>
           </Route>
